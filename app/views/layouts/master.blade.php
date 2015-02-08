5dcfb43246b0c41c93bf6763617124bd46aa131c
Thomas Nairn <thomas.nairn@thinkinnovate.co.uk>
2015-02-08 17:55:11 +0000

    Started work on a template using laravel

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,12 +3,22 @@
       <!--Import materialize.css-->
         @section('header')
           {{ HTML::style('css/materialize.css'); }}
+          {{ HTML::style('css/adjective.css'); }}
           <title>@yield('title','Adjective Laravel')</title>
         @show
     </head>
     <body>
         @section('sidebar')
         <ul id="slide-out" class="side-nav fixed">
+         <li id="logo">
+         <div  class="valign-wrapper">
+               <a href="#!"><h4 class="logo-title">Adjective</h4></a>
+               <div class="valign-wrapper" id="logo-icon">
+               <span class="valign logo-adjective">S</span>
+               </div>
+         </div>
+         <div class="clear"></div>
+         </li>
            <li><a href="#!">First Sidebar Link</a></li>
            <li><a href="#!">Second Sidebar Link</a></li>
            <li class="no-padding">

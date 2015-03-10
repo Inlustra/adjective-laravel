ab89e2b4c5b10f1ef8187b18299daafc41011c4c
bd218 <bd218@greenwich.ac.uk>
2015-03-10 17:07:23 +0000

    Create StaffSeederTable.php

---
+++ app/database/seeds/StaffSeederTable.php
@@ -0,0 +1,17 @@
+<?php
+class StaffTableSeeder extends Seeder {
+/**
+* Run the database seeds.
+*
+* @return void
+*/
+public function run()
+{
+$name = 'Lecturer';
+$role = 'LECTURER';
+$staff = new Staff;
+$staff->name = $name;
+$staff->role = $role;
+$staff->save();
+}
+}

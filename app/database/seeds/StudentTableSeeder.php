3281c8e9ed7db1ef475699ed58dfd19c3a943119
bd218 <bd218@greenwich.ac.uk>
2015-03-10 16:55:16 +0000

    Create StudentTableSeeder.php

---
+++ app/database/seeds/StudentTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+class StudentTableSeeder extends Seeder {
+/**
+* Run the database seeds.
+*
+* @return void
+*/
+public function run()
+{
+$degree = '1';
+$user = '1';
+$graduating = '2015/07/12';
+$student = new Student;
+$student->degree = $degree;
+$student->user = $user;
+$student->graduating = $graduating;
+$student->save();
+}
+}

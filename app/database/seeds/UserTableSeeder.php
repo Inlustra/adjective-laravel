e46b3a86f73734c444cc3ab163f770a33c13de70
Inlustra <nt206@greenwich.ac.uk>
2015-03-10 15:53:53 +0000

    Update UserTableSeeder.php

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -9,14 +9,36 @@ class UserTableSeeder extends Seeder {
    */
    public function run()
    {
-    $firstname = 'Thomas';
-    $lastname = 'Nairn';
+   	$firstNames = ['Idell','Wilbert','Neda','Harriet','Theo',
+   	'Ashly','Debi','Lois','Kristal','Nicki',
+   	'Vincenzo','Laurinda','Annmarie','Nakia',
+   	'Lettie','Stefany','Theola','Jeanmarie','Carli',
+   	'Ruthanne','Shila','Lavina','Shanae','Alina','Esperanza',
+   	'Nettie','Shawnee','Hui','Dusty','Melinda','Tamara','Talia',
+   	'Kassandra','Janie','Lashaun','Herbert','Dinorah','Kirstin',
+   	'Zofia','Isreal','Rosalee','Jacquelyne','Lyndia','Merna','Marilee'
+   	,'Albina','Torie','Anisha','Allie','Chara'];
+   	
+   	$lastNames = ['Salas','Rhodes','Moses','Larsen','Davies',
+   	'Lester','Armstrong','Wong','Taylor','Rice','Hester','Becker',
+   	'Ferrell','Hunter','Conner','Walters','Thompson','Newton','Fry',
+   	'Walsh','Cross','Herring','Wilcox','Browning','Salinas','Barron',
+   	'Whitney','Holloway','Obrien','Huber','Burnett','Sheppard','Sharp',
+   	'Mccann','Stevenson','Andrews','Knox','Castillo','Villa','Rodriguez',
+   	'Mcbride','Valentine','Ewing','Boyle','Kim','Brennan','Fisher',
+   	'Espinoza','Dickerson','Mcdowell'];
+   	
+   	for ($x = 0; $x <= 100; $x++) {
+    		$firstname = $firstNames[array_rand($firstNames)];
+    		$lastname = $lastNames[array_rand($lastNames)];
+    		
 		$user = new User;
 		$user->username = '$firstname.'.'.$lastname;
 		$user->firstname = $firstname
 		$user->lastname = $lastname
 		$user->password = Hash::make('test');
 		$user->save();
+		} 
    }
  
 }

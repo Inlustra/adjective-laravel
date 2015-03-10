cef26a4818302ebd6804c7fc4e03c119821c2a4f
bd218 <bd218@greenwich.ac.uk>
2015-03-10 16:56:56 +0000

    Update StudentTableSeeder.php

--- app/database/seeds/StudentTableSeeder.php
+++ app/database/seeds/StudentTableSeeder.php
@@ -15,5 +15,24 @@ $student->degree = $degree;
 $student->user = $user;
 $student->graduating = $graduating;
 $student->save();
+
+$degree = '1';
+$user = '2';
+$graduating = '2015/07/12';
+$student = new Student;
+$student->degree = $degree;
+$student->user = $user;
+$student->graduating = $graduating;
+$student->save();
+
+$degree = '1';
+$user = '3';
+$graduating = '2015/07/12';
+$student = new Student;
+$student->degree = $degree;
+$student->user = $user;
+$student->graduating = $graduating;
+$student->save();
+
 }
 }

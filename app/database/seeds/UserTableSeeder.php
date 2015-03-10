c43219f1b175b3cfcbe0c4835fd622ba10eb4c0f
bd218 <bd218@greenwich.ac.uk>
2015-03-10 16:31:16 +0000

    Update UserTableSeeder.php

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -9,36 +9,107 @@ class UserTableSeeder extends Seeder {
    */
    public function run()
    {
-   	$firstNames = ['Idell','Wilbert','Neda','Harriet','Theo',
-   	'Ashly','Debi','Lois','Kristal','Nicki',
-   	'Vincenzo','Laurinda','Annmarie','Nakia',
-   	'Lettie','Stefany','Theola','Jeanmarie','Carli',
-   	'Ruthanne','Shila','Lavina','Shanae','Alina','Esperanza',
-   	'Nettie','Shawnee','Hui','Dusty','Melinda','Tamara','Talia',
-   	'Kassandra','Janie','Lashaun','Herbert','Dinorah','Kirstin',
-   	'Zofia','Isreal','Rosalee','Jacquelyne','Lyndia','Merna','Marilee'
-   	,'Albina','Torie','Anisha','Allie','Chara'];
-   	
-   	$lastNames = ['Salas','Rhodes','Moses','Larsen','Davies',
-   	'Lester','Armstrong','Wong','Taylor','Rice','Hester','Becker',
-   	'Ferrell','Hunter','Conner','Walters','Thompson','Newton','Fry',
-   	'Walsh','Cross','Herring','Wilcox','Browning','Salinas','Barron',
-   	'Whitney','Holloway','Obrien','Huber','Burnett','Sheppard','Sharp',
-   	'Mccann','Stevenson','Andrews','Knox','Castillo','Villa','Rodriguez',
-   	'Mcbride','Valentine','Ewing','Boyle','Kim','Brennan','Fisher',
-   	'Espinoza','Dickerson','Mcdowell'];
-   	
-   	for ($x = 0; $x <= 100; $x++) {
-    		$firstname = $firstNames[array_rand($firstNames)];
-    		$lastname = $lastNames[array_rand($lastNames)];
-    		
-		$user = new User;
-		$user->username = $firstname.'.'.$lastname;
-		$user->firstname = $firstname
-		$user->lastname = $lastname
-		$user->password = Hash::make('test');
-		$user->save();
-		} 
-   }
- 
+$firstname = 'Lukas';
+$lastname = 'Bowden';
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->password = Hash::make('test');
+$user->save();
+
+$firstname = 'Daniel';
+$lastname = 'Bailey';
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->pa
+
+$firstname = 'Richard';
+$lastname = 'Barker';
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->password = Hash::make('test');
+$user->save();
+
+$firstname = 'Lee';
+$lastname = 'Johnson';
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->password = Hash::make('test');
+$user->save();
+
+$firstname = 'Zoe';
+$lastname = 'Luo';
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->password = Hash::make('test');
+$user->save();
+
+$firstname = 'Igor';
+$lastname = 'Jendoletov';
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->password = Hash::make('test');
+$user->save();
+
+$firstname = 'David';
+$lastname = 'Sime';
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->password = Hash::make('test');
+$user->save();
+
+$firstname = 'Thomas';
+$lastname = 'Nairn';
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->password = Hash::make('test');
+$user->save();
+
+$firstNames = ['Idell','Wilbert','Neda','Harriet','Theo',
+'Ashly','Debi','Lois','Kristal','Nicki',
+'Vincenzo','Laurinda','Annmarie','Nakia',
+'Lettie','Stefany','Theola','Jeanmarie','Carli',
+'Ruthanne','Shila','Lavina','Shanae','Alina','Esperanza',
+'Nettie','Shawnee','Hui','Dusty','Melinda','Tamara','Talia',
+'Kassandra','Janie','Lashaun','Herbert','Dinorah','Kirstin',
+'Zofia','Isreal','Rosalee','Jacquelyne','Lyndia','Merna','Marilee'
+,'Albina','Torie','Anisha','Allie','Chara'];
+
+$lastNames = ['Salas','Rhodes','Moses','Larsen','Davies',
+'Lester','Armstrong','Wong','Taylor','Rice','Hester','Becker',
+'Ferrell','Hunter','Conner','Walters','Thompson','Newton','Fry',
+'Walsh','Cross','Herring','Wilcox','Browning','Salinas','Barron',
+'Whitney','Holloway','Obrien','Huber','Burnett','Sheppard','Sharp',
+'Mccann','Stevenson','Andrews','Knox','Castillo','Villa','Rodriguez',
+'Mcbride','Valentine','Ewing','Boyle','Kim','Brennan','Fisher',
+'Espinoza','Dickerson','Mcdowell'];
+
+for ($x = 0; $x <= 100; $x++) {
+
+$firstname = $firstNames[array_rand($firstNames)];
+$lastname = $lastNames[array_rand($lastNames)];
+$user = new User;
+$user->username = $firstname.'.'.$lastname;
+$user->firstname = $firstname
+$user->lastname = $lastname
+$user->password = Hash::make('test');
+$user->save();
+
+}
+}
 }

5846c8619dcb32c93885086bcb03673050608f29
Thomas Nairn <thomas.nairn@thinkinnovate.co.uk>
2015-03-14 18:42:17 +0000

    Changed database password

--- app/database/seeds/DegreeTableSeeder.php
+++ app/database/seeds/DegreeTableSeeder.php
@@ -1,17 +1,18 @@
 <?php
- 
-class DegreeTableSeeder extends Seeder {
- 
-  /**
-   * Run the database seeds.
-   *
-   * @return void
-   */
-   public function run()
-   {
-$degree = new Degree;
-$degree->name = 'Computing';
-$degree->Director = '9';
-$degree->save();
-}
+
+class DegreeTableSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $degree = new Degree;
+        $degree->name = 'Computing';
+        $degree->Director = '9';
+        $degree->save();
+    }
 }

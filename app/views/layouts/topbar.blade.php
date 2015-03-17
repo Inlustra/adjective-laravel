4b7667e00fad0e1da44883cbb878af4d15cac054
smnsz <phw@Igors-iMac.local>
2015-03-17 03:14:16 +0000

    removed burger button

--- app/views/layouts/topbar.blade.php
+++ app/views/layouts/topbar.blade.php
@@ -4,37 +4,13 @@
 @if(Auth::user()->isStaff())
 <div class="nav-wrapper navstuff">
        <a href="#" class="brand-logo">Adjective | Staff</a>
-       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>                   
-	   <ul class="right hide-on-med-and-down">
-	   <li>{{ Auth::user()->username }}</li>
-       <li><a href="/logout">Logout</a></li>
-	   </ul>
-       
-       <ul class="side=nav" id="mobile-demo">
-       <li>{{ Auth::user()->username }}</li>
-       <li><a href="/logout">Logout</a></li>
-       </ul>
-</div>
-</nav>
-</div>
 
 @else
 <div class="nav-wrapper">
        <a href="#" class="brand-logo">Adjective | Student</a>       
        
+@endif 
 
-       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>                   
-	   <ul class="right hide-on-med-and-down">
-	   <li>{{ Auth::user()->username }}</li>
-       <li><a href="/logout">Logout</a></li>
-	   </ul>
-       
-       <ul class="side=nav" id="mobile-demo">
-       <li>{{ Auth::user()->username }}</li>
-       <li><a href="/logout">Logout</a></li>
-       </ul>
 </div>
 </nav>
 </div>
-
-@endif 

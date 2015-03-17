37f60377d5e6fd87c68518695ebf0a6beb9dea17
Inlustra <nt206@greenwich.ac.uk>
2015-03-17 15:38:14 +0000

    Update apiScaffold.blade.php

--- app/views/apiScaffold.blade.php
+++ app/views/apiScaffold.blade.php
@@ -5,7 +5,7 @@
 <a href="http://api.adjective.xyz/users">List all users</a><br/>
 <a href="http://api.adjective.xyz/users/students">List all students with degree and course data.</a><br/>
 <a href="http://api.adjective.xyz/users/students/course/1">List all students on the course with id 1</a><br/>
-<a href="http://api.adjective.xyz/users/students/course/Comput">List all students on the course beginning with "Comput"</a><br/>
+<a href="http://api.adjective.xyz/users/students/course/Final">List all students on the course beginning with "Final"</a><br/>
 <a href="http://api.adjective.xyz/users/students/degree/1">List all students on the degree with id 1</a><br/>
 <a href="http://api.adjective.xyz/users/students/degree/Comput">List all students on a degree beginning with "Comput"</a><br/>
 <a href="http://api.adjective.xyz/users/students/staff/">List all staff members with their associated staff data</a><br/>

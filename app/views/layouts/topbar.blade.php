3aa601d5efaa5bb863d10319c86c49c4ca0c28a3
smnsz <phw@Igors-iMac.local>
2015-03-17 03:06:00 +0000

    enough for today
    hopefully

--- app/views/layouts/topbar.blade.php
+++ app/views/layouts/topbar.blade.php
@@ -9,16 +9,14 @@
        <a href="#" class="brand-logo">Adjective | Student</a>       
        
 @endif 
-       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>            
-       <ul id="nav-mobile" class="right hide-on-med-and-down">
-	   <li>{{ Auth::user()->username }}</li>
-       <li><a href="/logout">Logout</a></li>
-       </ul>
+       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>                   
+	   <ul class="right hide-on-med-and-down">
+	   </ul>
+       
        <ul class="side=nav" id="mobile-demo">
        <li>{{ Auth::user()->username }}</li>
        <li><a href="/logout">Logout</a></li>
        </ul>
- 
-
+</div>
 </nav>
 </div>

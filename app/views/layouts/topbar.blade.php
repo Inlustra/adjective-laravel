0824d5b6aaa3b1fa31c2cd8a3dce0e16dbcd1dba
smnsz <phw@Igors-iMac.local>
2015-03-17 03:11:38 +0000

    last

--- app/views/layouts/topbar.blade.php
+++ app/views/layouts/topbar.blade.php
@@ -4,11 +4,6 @@
 @if(Auth::user()->isStaff())
 <div class="nav-wrapper navstuff">
        <a href="#" class="brand-logo">Adjective | Staff</a>
-@else
-<div class="nav-wrapper">
-       <a href="#" class="brand-logo">Adjective | Student</a>       
-       
-@endif 
        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>                   
 	   <ul class="right hide-on-med-and-down">
 	   <li>{{ Auth::user()->username }}</li>
@@ -22,3 +17,24 @@
 </div>
 </nav>
 </div>
+
+@else
+<div class="nav-wrapper">
+       <a href="#" class="brand-logo">Adjective | Student</a>       
+       
+
+       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>                   
+	   <ul class="right hide-on-med-and-down">
+	   <li>{{ Auth::user()->username }}</li>
+       <li><a href="/logout">Logout</a></li>
+	   </ul>
+       
+       <ul class="side=nav" id="mobile-demo">
+       <li>{{ Auth::user()->username }}</li>
+       <li><a href="/logout">Logout</a></li>
+       </ul>
+</div>
+</nav>
+</div>
+
+@endif 

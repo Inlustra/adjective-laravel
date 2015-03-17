90144f519fdc0a6008d08462dbbeec3fb4aed541
Inlustra <nt206@greenwich.ac.uk>
2015-03-17 15:34:34 +0000

    Update apiScaffold.blade.php

--- app/views/apiScaffold.blade.php
+++ app/views/apiScaffold.blade.php
@@ -1,1 +1,16 @@
-
+<html>
+<body>
+<h2>Your data, your way.</h2>
+<a href="http://api.adjective.xyz/team">The team (Take the first 10 users)</a><br/>
+<a href="http://api.adjective.xyz/users">List all users</a><br/>
+<a href="http://api.adjective.xyz/users/students">List all students with degree and course data.</a><br/>
+<a href="http://api.adjective.xyz/users/students/course/1">List all students on the course with id 1</a><br/>
+<a href="http://api.adjective.xyz/users/students/course/Comput">List all students on the course beginning with "Comput"</a><br/>
+<a href="http://api.adjective.xyz/users/students/degree/1">List all students on the degree with id 1</a><br/>
+<a href="http://api.adjective.xyz/users/students/degree/Comput">List all students on a degree beginning with "Comput"</a><br/>
+<a href="http://api.adjective.xyz/users/students/staff/">List all staff members with their associated staff data</a><br/>
+<a href="http://api.adjective.xyz/users/students/staff/Lecturer">List all staff members with a staff role named "Lecturer"</a><br/>
+<a href="http://api.adjective.xyz/users/Thomas">Get the first user with the name Thomas</a><br/>
+<a href="http://api.adjective.xyz/users/Thomas/conversation">Get the first user with the name Thomas, show all his conversations.</a><br/>
+</body>
+</html>

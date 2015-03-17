c499c627fe67812947d740a95e070c4ce7266b7b
smnsz <phw@Igors-iMac.local>
2015-03-17 00:54:59 +0000

    commit
    change icons on latest messages list
    removing space after messages

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -23,10 +23,11 @@
 <div class="divider"></div>
 
 <div class="container">
-<div class="row">   
+<div class="row">
+	<div class="col s12"><h4><i class="small mdi-communication-message"></i> Latest Messages</h4>   
 	<ul class="collapsible" data-collapsible="accordion">
     <li>
-      <div class="collapsible-header"><i class="mdi-image-filter-drama"></i>Ray Stoneham</div>
+      <div class="collapsible-header"><i class="mdi-communication-message"></i>Ray Stoneham</div>
       <div class="collapsible-body"><p>How are u today?</p>
       <p><button class="btn waves-effect waves-light" type="submit" name="action">Reply
     <i class="mdi-content-send right"></i>
@@ -35,7 +36,7 @@
       </div>
     </li>
     <li>
-      <div class="collapsible-header"><i class="mdi-maps-place"></i>Keeran Jamil</div>
+      <div class="collapsible-header"><i class="mdi-communication-message"></i>Keeran Jamil</div>
       <div class="collapsible-body"><p>Coursework is going somewhere.</p>
       <p><button class="btn waves-effect waves-light" type="submit" name="action">Reply
     <i class="mdi-content-send right"></i>
@@ -51,13 +52,14 @@
      </div>
 </div> 
 
-<br>
-
 <div class="divider"></div>
 
-                   
+<div class="container">
+<div class="row">
+	<div class="col s12"><h4><i class="small mdi-av-my-library-books"></i> Courses</h4>                      
                             
 
-                       
+	</div>
+</div>                       
  
 @stop

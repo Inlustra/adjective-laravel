2a790539a2ec929133db41bd8f0e4880e576be0b
smnsz <phw@Igors-iMac.local>
2015-03-17 00:22:12 +0000

    sidenav

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -24,17 +24,6 @@
 <div class="container">
 <div class="row">   
      <div class="col s12"><h4><i class="small mdi-communication-message"></i> Latest Messages</h4>
-	 <ul class="collection with-header">
-        <li class="collection-header"><h4>Ray Stoneham</h4></li>
-        <li class="collection-item"><div>How are you</div></li>     
-		<li class="collection-item"><div>I'm good and you<a href="#!" class="secondary-content"><i class="mdi-content-send"></i></a></div></li>
-	 </ul>
-	 <div class="col s12"><h4><i class="small mdi-communication-message"></i> Latest Messages</h4>
-	 <ul class="collection with-header">
-        <li class="collection-header"><h4>Keeran Jamiil</h4></li>
-        <li class="collection-item"><div>Coursework is it done</div></li>     
-		<li class="collection-item"><div>Still nothing<a href="#!" class="secondary-content"><i class="mdi-content-send"></i></a></div></li>
-	 </ul>
 	 <ul class="collection">
 	 <li class="collection-item avatar">
       <i class="mdi-file-folder circle"></i>

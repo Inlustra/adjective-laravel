7c69e626a0b6b70219abbb4b7d3014a5962c139e
smnsz <phw@Igors-iMac.local>
2015-03-17 01:02:20 +0000

    corrections

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -24,19 +24,18 @@
 
 <div class="container">
 <div class="row">
-	<div class="col s12"><h4><i class="small mdi-communication-message"></i> Latest Messages</h4>   
+	<div class="col s12"><h4><i class="small mdi-communication-message"></i> Latest Messages</h4>
+	</div>
+	   
 	<ul class="collapsible" data-collapsible="accordion">
     <li>
-      <div class="collapsible-header"><i class="mdi-communication-message"></i>Ray Stoneham</div>
-      <div class="collapsible-body"><p>How are u today?</p>
-      <p><button class="btn waves-effect waves-light" type="submit" name="action">Reply
-    <i class="mdi-content-send right"></i>
-  </button>
-      
-      </div>
-    </li>
+    <div class="collapsible-header"><i class="mdi-communication-email"></i>Ray Stoneham</div>
+    <div class="collapsible-body"><p>How are u today?</p>
+    <p><button class="btn waves-effect waves-light" type="submit" name="action">Reply
+    <i class="mdi-content-send right"></i></button></p></div> 
+
     <li>
-      <div class="collapsible-header"><i class="mdi-communication-message"></i>Keeran Jamil</div>
+      <div class="collapsible-header"><i class="mdi-communication-email"></i>Keeran Jamil</div>
       <div class="collapsible-body"><p>Coursework is going somewhere.</p>
       <p><button class="btn waves-effect waves-light" type="submit" name="action">Reply
     <i class="mdi-content-send right"></i>

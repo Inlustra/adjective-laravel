146ec6c957a5736f6051c0d6109028fd7f85336a
smnsz <phw@Igors-iMac.local>
2015-03-28 18:22:53 +0000

    announcements

--- app/views/travel.blade.php
+++ app/views/travel.blade.php
@@ -6,7 +6,7 @@
   
 <div class="container">
 <div class="row">
-	<div class="col s12"><h5><i class="small mdi-maps-directions-bus"></i> Travel details</h5>
+	<div class="col s12"><h4><i class="small mdi-maps-directions-bus"></i> Travel details</h4>
 	</div>
 	
 content 		

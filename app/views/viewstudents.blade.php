fb3ab764eb1bdb3f09761d1cdcca48537ac51d3f
smnsz <phw@Igors-iMac.local>
2015-03-29 15:07:20 +0100

    viewstudents

--- app/views/viewstudents.blade.php
+++ app/views/viewstudents.blade.php
@@ -10,27 +10,25 @@
 	</div>
 	
 	<div class="table-responsive">
-        <table class="table table-bordered table-striped">
+        <table class="table table-bordered table-striped table-text">
  
             <thead>
                 <tr>
                     <th>Name</th>
-                    <th>Username</th>
                     <th>Email</th>
-                    <th>First Marker</th>
-                    <th>Second Marker</th>
+                    <th>1st Marker</th>
+                    <th>2nd Marker</th>
                     <th></th>
                 </tr>
             </thead>
             
             <tbody>
 				<tr>
-					<td>Firstname</td>
-					<td>Username</td>
-					<td>Email@email</td>
-					<td></td>
-					<td></td>
-					<td>button edit</td>
+					<td>Igor Jendoletov</td>
+					<td>ji204@greenwichac.uk</td>
+					<td>Keeran Jamil</td>
+					<td>Ray Stoneham</td>
+					<td><div class="btn btn-small"><span>Edit</span></div></td>
             </tr>
             </tbody>
            

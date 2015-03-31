9b5be3ff1d92a6b56e805a6e00efa22d1314d89c
smnsz <phw@Igors-iMac.local>
2015-03-31 01:14:55 +0100

    viewuploads
    tags ended properly

--- app/views/viewuploads.blade.php
+++ app/views/viewuploads.blade.php
@@ -3,12 +3,39 @@
 @section('title') Student Editing Details @stop
 
 @section('content')
+
     <div class="container">
         <div class="row">
-            <div class="col s12"><h4><i class="small mdi-action-backup"></i> View all your uploads</h4>
+            <div class="col s12"><h4><i class="small mdi-action-backup"></i> View Students</h4>
             </div>
-            content
+
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped table-text">
+                    <thead>
+                    <tr>
+                        <th>Filename</th>
+                        <th>Course</th>
+                        <th>Type</th>
+                        <th>Date</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Filename</td>
+                        <td>Course Name</td>
+                        <td>Interim</td>
+                        <td>26.03.2015</td>
+                        <td>
+                            <div class="btn btn-small"><span>Delete</span></div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     </div>
     </div>
+
 @stop

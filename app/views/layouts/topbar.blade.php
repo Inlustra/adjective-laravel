48062b804003a9d797f3fbe58c3c449862df58a2
smnsz <phw@Igors-iMac.local>
2015-03-31 02:45:13 +0100

    topbar

--- app/views/layouts/topbar.blade.php
+++ app/views/layouts/topbar.blade.php
@@ -1,8 +1,8 @@
 <div class="navbar-fixed">
     <nav>
         @if(Auth::user()->isStaff())
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo">Adjective | Student</a>
+            <div class="nav-wrapper navstuff">
+                <a href="#" class="brand-logo">Adjective | Stuff</a>
             </div>
         @else
             <div class="nav-wrapper">

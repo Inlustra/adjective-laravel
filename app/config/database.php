ab4fc557c3552a00a969f47bdfc4e9a2d47f02e0
Thomas Nairn <thomas.nairn@thinkinnovate.co.uk>
2015-04-01 00:24:55 +0100

    Databse fix oops

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'adjective',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'QLUVRSxQOP',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

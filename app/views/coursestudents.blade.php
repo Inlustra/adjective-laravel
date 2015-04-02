585e15b3621d53ba00f5877a162b11edfc9c7400
smnsz <phw@Igors-iMac.local>
2015-04-02 21:43:48 +0100

    coursestudent

--- app/views/coursestudents.blade.php
+++ app/views/coursestudents.blade.php
@@ -27,7 +27,7 @@
                         @foreach($course->students as $student)
                             @if($student->SecondMarker!= null && $student->Supervisor != null)
                                 <tr>
-                                    <td><form><input type="checkbox" id="testing" /></form></td>
+                                    <td><form action="#"><input type="checkbox" id="test1" /><label for="test1"></label></form></td>
                                     <td>{{$student->user->fullName}}</td>
                                     <td>{{$student->user->email}}</td>
                                     <td>{{$student->Supervisor->fullName  or 'None'}}</td>

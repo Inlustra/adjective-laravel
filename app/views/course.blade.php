6df31b008b6d40cda62d88016e4993b681f76b3a
smnsz <phw@Igors-iMac.local>
2015-04-02 21:36:36 +0100

    course blade

--- app/views/course.blade.php
+++ app/views/course.blade.php
@@ -190,6 +190,7 @@
                             <table class="table table-bordered table-striped table-text">
                                 <thead>
                                 <tr>
+                                	<th></th>
                                     <th>Name</th>
                                     <th>Email</th>
                                     <th>1st Marker</th>
@@ -200,6 +201,7 @@
                                 <tbody>
                                 @foreach($course->students as $student)
                                     <tr>
+                                    	<td><input type="checkbox" id="test5" /></td>
                                         <td>{{$student->user->fullName}}</td>
                                         <td>{{$student->user->email}}</td>
                                         <td>{{$student->Supervisor->fullName  or 'None'}}</td>
